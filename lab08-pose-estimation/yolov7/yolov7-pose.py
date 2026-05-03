c935e6e61ab53aa9a7b4fb741cffd8eb802b1312
faihadiln-spec <faihadi.ln@gmail.com>
2026-05-03 03:28:25 +0300

    Update yolov7-pose.py

--- lab08-pose-estimation/yolov7/yolov7-pose.py
+++ lab08-pose-estimation/yolov7/yolov7-pose.py
@@ -70,11 +70,10 @@ _ = model.float().eval()
 model.to(device)
 
 # Provide the list of paths to your chosen videos her
-videos = [
-        'skydiving',
-        'far-away']
+videos = ['yoga']
 
 file_name = videos[0] + '.mp4'
+save_name = videos[0]
 vid_path = '../media/' + file_name
 
 cap = cv2.VideoCapture(vid_path)
@@ -87,8 +86,12 @@ h, w, _ = frame.shape
 #                       cv2.VideoWriter_fourcc(*'mp4v'), 
 #                       fps, (w, h))
 
-out = cv2.VideoWriter(f"{save_name}_yolo7.avi",cv2.VideoWriter_fourcc('M','J','P','G'), 10, w,h)
-
+out = cv2.VideoWriter(
+    f"{save_name}_yolov7.avi",
+    cv2.VideoWriter_fourcc('M','J','P','G'),
+    10,
+    (w, h)
+)
 #-------------------------------------------------------------------------------#
 
 
